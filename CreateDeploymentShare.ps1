ce12574f7d6d0cea23690196652744ba916ea5e1
mhoppeats <michael.hoppe@ats-global.com>
2017-08-17 13:18:19 +0200

    change share name

--- CreateDeploymentShare.ps1
+++ CreateDeploymentShare.ps1
@@ -2,7 +2,7 @@ netsh advfirewall firewall set rule group="File and Printer Sharing" new enable=
 
     # Configure SMB Share and Set permissions
     New-Item -ItemType Directory -Force -Path "C:\Deploy" 
-    if(!(Get-SMBShare -Name "Share" -ea 0)){
-          New-SMBShare -Name "Share" -Path "C:\Deploy" -Description "Deployment Directory"
+    if(!(Get-SMBShare -Name "Deploy" -ea 0)){
+          New-SMBShare -Name "Deploy" -Path "C:\Deploy" -Description "Deployment Directory"
     }
-    Get-SmbShareAccess -Name "Share" | Grant-SmbShareAccess -AccountName Everyone -AccessRight Full -Force
+    Get-SmbShareAccess -Name "Deploy" | Grant-SmbShareAccess -AccountName Everyone -AccessRight Full -Force
